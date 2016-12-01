27780d3d1aac3286647abe2f2eeee93844cd838f
Ian Li <ianli6362@gmail.com>
2016-12-01 09:44:55 -0800

    ContentInset NavigationBar

--- LegendOfHeroVI/ViewController.swift
+++ LegendOfHeroVI/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        
     }
 
     override func didReceiveMemoryWarning() {
